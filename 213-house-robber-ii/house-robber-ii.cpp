74253e4fb0295d5d01a408c0b07bdb5ef35df047
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-08-07 21:19:03 +0530

    Time: 2 ms (8.78%) | Memory: 10.9 MB (38.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int rob(vector<int>& cost) {
-        int n = cost.size();
-        int ans = 0;
+    int rob(vector<int>& nums) {
+       // 0 to n-1 houses, rob house 0 to n-2 and rob houses 1 to n-1, report max of two
+       int n = nums.size();
+       if(n == 1) return nums[0];
+       if(n == 2) return max(nums[0], nums[1]);
+       vector<int> dp(n, -1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        int ans = dp[1];
 
-        if(n == 0) return 0;
-        if(n == 1) return cost[0];
-
-        // robbing from house 0 to n-2
-        vector<int> dp(n, 0);
-        dp[0] = 0;
-        dp[1] = cost[0];
-
-        for(int i=2; i<n; i++) {
-            dp[i] = max(dp[i-1], cost[i-1]+dp[i-2]);
+        for(int i=2; i<n-1; i++) {
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
         }
+        ans = dp[n-2];
 
-        ans = max(ans, dp[n-1]);
+        fill(dp.begin(), dp.end(), 0);
+        dp[1] = nums[1];
+        dp[2] = max(nums[2], nums[1]);
 
-        // robbing house from 1 to n-1
-        dp[0] = 0;
-        dp[1] = cost[1];
-
-        for(int i=2; i<n; i++) {
-            dp[i] = max(dp[i-1], cost[i]+dp[i-2]);
+        for(int i=3; i<n; i++) {
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
         }
-
         ans = max(ans, dp[n-1]);
         return ans;
     }
